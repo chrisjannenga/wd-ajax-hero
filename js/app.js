a2134f57e0304bdd993e167895672b2d23fde6ab
Chris Jannenga <Cjannenga312@gmail.com>
2017-07-13 10:19:55 -0700

    Functional

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@
     $('#listings').empty();
 
     for (const movie of movies) {
+      console.log(movie);
       const $col = $('<div>').addClass('col s6');
       const $card = $('<div>').addClass('card hoverable');
       const $content = $('<div>').addClass('card-content center');
@@ -14,16 +15,18 @@
 
       $title.attr({
         'data-position': 'top',
-        'data-tooltip': movie.title
+        'data-tooltip': movie.Title
       });
 
-      $title.tooltip({ delay: 50 }).text(movie.title);
+      $title.tooltip({
+        delay: 50
+      }).text(movie.Title);
 
       const $poster = $('<img>').addClass('poster');
 
       $poster.attr({
-        src: movie.poster,
-        alt: `${movie.poster} Poster`
+        src: movie.Poster,
+        alt: `${movie.Poster} Poster`
       });
 
       $content.append($title, $poster);
@@ -33,17 +36,17 @@
       const $plot = $('<a>');
 
       $plot.addClass('waves-effect waves-light btn modal-trigger');
-      $plot.attr('href', `#${movie.id}`);
+      $plot.attr('href', `#${movie.imdbID}`);
       $plot.text('Plot Synopsis');
 
       $action.append($plot);
       $card.append($action);
 
-      const $modal = $('<div>').addClass('modal').attr('id', movie.id);
+      const $modal = $('<div>').addClass('modal').attr('id', movie.imdbID);
       const $modalContent = $('<div>').addClass('modal-content');
       const $modalHeader = $('<h4>').text(movie.title);
-      const $movieYear = $('<h6>').text(`Released in ${movie.year}`);
-      const $modalText = $('<p>').text(movie.plot);
+      const $movieYear = $('<h6>').text(`Released in ${movie.Year}`);
+      const $modalText = $('<p>').text(movie.Plot);
 
       $modalContent.append($modalHeader, $movieYear, $modalText);
       $modal.append($modalContent);
@@ -57,4 +60,30 @@
   };
 
   // ADD YOUR CODE HERE
+
+  function clearInputs() {
+    $("#search").val('');
+    console.log("User input cleared");
+  }
+
+  $('button').click(function() {
+    event.preventDefault();
+    movies.length = 0;
+    var $movie = $('#search').val();
+    if (!$('#search').val()) {
+      alert("please enter search text");
+    } else {
+      $.ajax({
+        url: "http://www.omdbapi.com/?s=" + $("#search").val() + "&apikey=702b3bb5",
+        success: function(data) {
+          var searchDataArry = data["Search"];
+          for (var i = 0; i < searchDataArry.length; i++) {
+            movies.push(searchDataArry[i]);
+            renderMovies();
+          }
+
+        }
+      })
+    }
+  })
 })();
